4ba556ee71d453c1b8b650a15e648914c3ee0ecc
AlexxBG <sashosv2007@gmail.com>
2026-02-13 09:50:31 +0200

    Update game.js

--- Game/game.js
+++ Game/game.js
@@ -1,12 +1,23 @@
 const canvas = document.getElementById("gameCanvas");
 const context = canvas.getContext("2d");
 
+canvas.width  = window.innerWidth - 20;
+canvas.height = window.innerHeight - 20;
+
+let timeLast  = 0 ;
+const targetFPS = 120;
 const playerWidth = 80;
 const playerHeight = 100;
-let playerX = 400;
-let playerY = 300;
+let playerStart = {
+    x: canvas.width / 2,
+    y: canvas.height / 2
+};
+
+let playerX = playerStart.x;
+let playerY = playerStart.y;
+
 let playerSpeed = 5;
-let playerStart = { x: 400, y: 300 };
+
 const playerImage = new Image();
 playerImage.src = "Imagesgame/potato.png";
 playerImage.onload = () => {
@@ -20,39 +31,54 @@ let buttonDDown = false;
 let buttonA_WDown = false;
 let buttonD_Wown = false;
 let buttonA_SDown = false;
-let buttonA_own = false;
-let buttonRESETDown = false;
+let buttonD_SDown = false;
+let buttonResetDown = false;
+//let buttonShiftDown = false;
 
 addEventListener("keydown", (event) => {
-
-if (event.key === "w")
+ const key = event.key.toLowerCase();
+if (event.key === "w" || event.key === "в"  || event.key === "у")
     buttonWDown = true;
-    if (event.key === "a")
+    if (event.key === "a" || event.key === "а"  || event.key === "ь")
     buttonADown = true;
-    if (event.key === "s")
+    if (event.key === "s" || event.key === "с"  || event.key === "я")
     buttonSDown = true;
-    if (event.key === "d")
+    if (event.key === "d" || event.key === "д"  || event.key === "а")
     buttonDDown = true;
-if (event.key === "r")
-    buttonRESETDown = true;
+    if (event.key === "r" || event.key === "р"  || event.key === "и")
+    buttonResetDown = true;
+   // if (event.key === "ShiftLeft")
+   // buttonShiftDown = true;
+
 });
 
 addEventListener("keyup", (event) => {
-    if (event.key === "w")
+   
+    if (event.key === "w" || event.key === "в"  || event.key === "у")
     buttonWDown = false;
-    if (event.key === "a")
+    if (event.key === "a" || event.key === "а"  || event.key === "ь")
     buttonADown = false;
-    if (event.key === "s")
+    if (event.key === "s" || event.key === "с"  || event.key === "я")
     buttonSDown = false;
-    if (event.key === "d")
+    if (event.key === "d" || event.key === "д" || event.key === "а")
     buttonDDown = false;
-if (event.key === "r")
-    buttonRESETDown = false;
+    if (event.key === "r" || event.key === "р" || event.key === "и")
+    buttonResetDown = false;
+   // if (event.key === "ShiftLeft")
+   // buttonShiftDown = false;
 
 });
-function update()
+function update(timeCurrent)
 {  
-    if (buttonRESETDown){
+    const deltaTime = timeCurrent - timeLast;
+    const singleFrameTime = 1000/ targetFPS;
+    if (deltaTime < singleFrameTime){
+        console.log("delaying")
+        return
+    }
+    console.log(deltaTime)
+    
+if (buttonResetDown){
     playerX = playerStart.x;
     playerY = playerStart.y;
     }
@@ -64,26 +90,23 @@ function update()
         playerY += playerSpeed;
     if (buttonDDown)
         playerX += playerSpeed;
+    //if(buttonShiftDown)
+    //    playerSpeed += 5
    
  
 if (playerX + playerWidth / 2 >= canvas.width) {
     playerX = canvas.width - playerWidth / 2;
 }
-
-
 if (playerX - playerWidth / 2 <= 0) {
     playerX = playerWidth / 2;
 }
-
-
 if (playerY + playerHeight / 2 >= canvas.height) {
     playerY = canvas.height - playerHeight / 2;
 }
-
-
 if (playerY - playerHeight / 2 <= 0) {
     playerY = playerHeight / 2;
 }
+
 context.clearRect(0,0,canvas.width,canvas.height)
 context.drawImage(
     playerImage,
@@ -92,8 +115,11 @@ context.drawImage(
     playerWidth,
     playerHeight
     );
-
+timeLast = timeCurrent;
+//8.3
     requestAnimationFrame(update);
 
 }
+requestAnimationFrame(update);
+
 
